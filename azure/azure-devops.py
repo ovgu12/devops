001d547b24e444472ec1bca02557aca0c0ac1d80
Hoang Vu <hoangvuminh@hotmail.de>
2024-03-13 21:54:18 +0100

    Adds azure-pipeline

---
+++ azure/azure-devops.py
@@ -0,0 +1,22 @@
+import os
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+
+print(os.environ.get('SYSTEM_ACCESSTOKEN'))
+
+# Fill in with your personal access token and org URL
+personal_access_token = os.environ.get('SYSTEM_ACCESSTOKEN')
+organization_url = 'https://dev.azure.com/Okarben'
+
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client (the "core" client provides access to projects, teams, etc)
+core_client = connection.clients.get_core_client()
+
+# Get projects
+get_projects_response = core_client.get_projects()
+
+for project in get_projects_response:
+    print(project.name)
